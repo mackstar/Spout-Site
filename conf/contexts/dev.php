3b0f469e21c35f9a3bcbb8b72c77ce0c49b612fc
Richard McIntyre <richard.mackstar@gmail.com>
2014-10-02 19:40:29 +0100

    Putting SQLIte as dev db

--- conf/contexts/dev.php
+++ conf/contexts/dev.php
@@ -4,8 +4,18 @@
  * Welcome to the Mackstar.Spout Project
  *
  * This is where you can override the default configuration.
- * for the *DEV* context.
+ * for the *TEST* context. 
  */
 
 return [
+    'master_db' => [
+        'driver' => 'pdo_sqlite',
+        'path' => dirname(dirname(__DIR__)) . '/test_db.sqlite3',
+        'charset' => 'UTF8'
+    ],
+    'slave_db' => [
+        'driver' => 'pdo_sqlite',
+        'path' => dirname(dirname(__DIR__)) . '/test_db.sqlite3',
+        'charset' => 'UTF8'
+    ]
 ];
